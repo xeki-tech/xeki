03111864c9e3c932cafa3688c4d7e0e4fef26776
Luis Eduardo Patt <liuspatt@gmail.com>
2021-07-20 19:44:02 -0500

    clear and set examples urls

--- app/core/controllers/home.php
+++ app/core/controllers/home.php
@@ -1,5 +1,2 @@
 <?php
-$html = \xeki\module_manager::import_module('html-twig');
-$html->render('home.twig', []);
-
-//\xeki\core::PrintJson(["response"=>'ok',"date"=>date("Y-m-d")]);
+\xeki\core::PrintJson(["response"=>'ok',"date"=>date("Y-m-d")]);
